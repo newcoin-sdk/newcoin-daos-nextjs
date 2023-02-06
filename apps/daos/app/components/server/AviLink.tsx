23dea615bc3a72c332af9df217aee5e6aa7b2714
yannafaithbot <kayanna_chandler@woebothealth.com>
2023-02-06 18:51:10 -0500

    style: checkpoint for ideating a new Dao Header look

--- apps/daos/app/components/server/AviLink.tsx
+++ apps/daos/app/components/server/AviLink.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Header3 from "@/app/components/server/Header3";
 import pic from "../../../public/pic.png";
+import Header1r from "@/app/components/server/Header1r";
 
-//@ts-ignore
-export default function AviLink () {
+export function AviLinkSmall () {
 	return (
-			<div className={"flex flex-row items-center justify-end mt-10"}>
+		<div className={"flex flex-row items-center"}>
 				<Image
 					className={"rounded-full mr-5 w-12 h-12"}
 					src={pic}
@@ -14,5 +14,22 @@ export default function AviLink () {
 				/>
 				<Header3>{"test user"}</Header3>
 			</div>
+	);
+}
+
+//@ts-ignore
+export function AviLinkLarge({ children }) {
+	return (
+		<div className={"flex flex-row items-center justify-end mt"}>
+			<Image
+				className={"rounded-full mr-10"}
+				src={pic}
+				alt={"avatar"}
+				priority={true}
+				width={"1rem"}
+				height={"1rem"}
+			/>
+			<Header1r>{ children }</Header1r>
+		</div>
 	);
 }
